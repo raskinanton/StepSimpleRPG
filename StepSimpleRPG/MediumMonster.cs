c882ded974962c6b427115dfa7c2a2e736968897
jeff9@yandex.ru <jeff9@yandex.ru>
2020-10-18 16:52:16 +0300

    add class MediumMonster

---
+++ StepSimpleRPG/MediumMonster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepSimpleRPG
+{
+    class MediumMonster: Monster
+    {
+        List<IItem> _items;
+    }
+}
