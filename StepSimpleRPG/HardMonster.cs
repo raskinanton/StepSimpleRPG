f9e5af16072f47b01bd3a1aa79df9a61903a4b14
jeff9@yandex.ru <jeff9@yandex.ru>
2020-10-18 17:27:37 +0300

    change MediumMonster and HardMonster

--- StepSimpleRPG/HardMonster.cs
+++ StepSimpleRPG/HardMonster.cs
@@ -10,5 +10,19 @@ namespace StepSimpleRPG
     {
         List<IItem> _items;
 
+        public HardMonster()
+        {
+            specs.Name = "HardMonster";
+            specs.Health = 30;
+            specs.Coin = 3;
+            specs.Armor = 10;
+            specs.Exp = 20;
+        }
+
+        public override string ToString()
+        {
+            return $"{specs.Name}, health: {specs.Health}, coin: {specs.Coin}, armor: {specs.Armor}, Exp:{ecs.Exp}";
+        }
+
     }
 }
--- StepSimpleRPG/MediumMonster.cs
+++ StepSimpleRPG/MediumMonster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,18 @@ namespace StepSimpleRPG
     {
         List<IItem> _items;
 
+        public  MediumMonster()
+        {
+            specs.Name = "MediumMonster";
+            specs.Health = 20;
+            specs.Coin = 2;
+            specs.Armor = 5;
+            specs.Exp = 10;
+        }
+
+        public override string ToString()
+        {
+            return $"{specs.Name}, health: {specs.Health}, coin: {specs.Coin}, armor: {specs.Armor}, Exp:{ecs.Exp}";
+        }
     }
 }
