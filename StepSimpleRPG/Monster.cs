1e3eb1b33d5a04330da698d943d30a284ac9a3a6
Виктор Лукашев <lukashev-viktor2012@yandex.ru>
2020-10-18 16:42:28 +0300

    Add Class Monster

---
+++ StepSimpleRPG/Monster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepSimpleRPG
+{
+    class Monster 
+    {
+        private int damage;
+        private int exp;
+        private int coins;
+        private int PassCost;
+    }
+}
+
