84c8512c404a5b770be0f037b5ec8089bdd02d09
Александр Сахатырь <alexandr.010@yandex.ru>
2020-10-18 16:36:37 +0300

    [WIP] tmp

---
+++ StepSimpleRPG/Armor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepSimpleRPG
+{
+    class Armor : IItem
+    {
+        public void Apply()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
