b801ba75b7a06cfd756eecbef8c90548039235ac
theraconteur <raskin.anton.v@gmail.com>
2020-10-18 17:24:08 +0300

    Player moved to players folder

--- StepSimpleRPG/Players/Player.cs
+++ StepSimpleRPG/Players/Player.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using StepSimpleRPG.Items;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StepSimpleRPG
+namespace StepSimpleRPG.Players
 {
     class Player : IPlayer
     {
